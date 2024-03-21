9aeb916c964a97b6908916de024b59aaf036904e
Pouya Ghajardavalou <ghajar.pouya@gmail.com>
2024-03-21 10:03:50 -0700

    file added

---
+++ ft_strncat.c
@@ -0,0 +1,31 @@
+int	get_length(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	if (nb < 1)
+	{
+		return (dest);
+	}
+	i = 0;
+	j = get_length(dest);
+	while (src[i] != '\0' && i < nb)
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
